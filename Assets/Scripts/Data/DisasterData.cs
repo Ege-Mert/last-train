2e3d8789fff73720e30c456efa6dec0502dc1e8e
Ege Mert <70327391+Ege-Mert@users.noreply.github.com>
2024-12-16 06:06:34 +0300

    Revert "Feature 2/wagons"

--- Assets/Scripts/Data/DisasterData.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "DisasterData", menuName = "last-train/DisasterData")]
-public class DisasterData : ScriptableObject
-{
-    public float initialSpeed = 0.5f;
-    public float accelerationRate = 0.01f;
-}
--- Assets/Scripts/Events/EventEffect.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public abstract class EventEffect : ScriptableObject
-{
-    public abstract void Apply(GameManager gameManager);
-}
